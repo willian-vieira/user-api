62f7a54bbe7d30f0b2f7fb943fbd1b0de47a892c
willian-vieira <williannvieira.w@gmail.com>
2024-02-03 22:57:12 -0300

    feat: Criação e Implementação dos Métodos UserService

--- src/main/java/apiuser/service/UserService.java
+++ src/main/java/apiuser/service/UserService.java
@@ -7,10 +7,52 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class UserService {
     @Autowired
     IUserRepository userRepository;
+
+    public List<UserDTO> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users
+            .stream()
+            .map(UserDTO::convertToUser)
+            .collect(Collectors.toList());
+    }
+
+    public UserDTO getUserById(long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user
+            .map(UserDTO::convertToUser)
+            .orElse(null);
+    }
+
+    public UserDTO getUserByCpf(String cpf) {
+        User user = userRepository.findByCpf(cpf);
+        if (user != null) {
+            return UserDTO.convertToUser(user);
+        }
+        return null;
+    }
+
+    public List<UserDTO> queryByNameUserLike(String nome) {
+        List<User> users = userRepository.findByNomeContains(nome);
+        return users
+            .stream()
+            .map(UserDTO::convertToUser)
+            .collect(Collectors.toList());
+    }
+
+    public UserDTO createUser(UserDTO userDTO) {
+        User user = userRepository.save(User.convertToUserDTO(userDTO));
+        return UserDTO.convertToUser(user);
+    }
+
+    public void deleteUser(long userId) {
+        Optional<User> user = userRepository.findById(userId);
+        user.ifPresent(value -> userRepository.delete(value));
+    }
 }
