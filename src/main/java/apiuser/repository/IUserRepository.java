901e8bd065e181195d5b97701ee9ab37139f82de
willian-vieira <williannvieira.w@gmail.com>
2024-02-03 22:39:28 -0300

    feat: Criado Métodos de Busca Personalizados - (JPA)

--- src/main/java/apiuser/repository/IUserRepository.java
+++ src/main/java/apiuser/repository/IUserRepository.java
@@ -3,5 +3,9 @@ package apiuser.repository;
 import apiuser.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IUserRepository extends JpaRepository<User, Long> {
+    User findByCpf(String cpf);
+    List<User> findByNomeContains(String nome);
 }
