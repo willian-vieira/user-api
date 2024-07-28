729eb28d570efb01684f27843a69ed180c2b4177
willian-vieira <williannvieira.w@gmail.com>
2024-07-28 16:23:47 -0300

    feat: Classe UserDTO Criada

---
+++ src/main/java/com/user_api/dto/UserDTO.java
@@ -0,0 +1,31 @@
+package com.user_api.dto;
+
+import com.user_api.entity.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class UserDTO {
+    private Long id;
+    private String name;
+    private String cpf;
+    private String address;
+    private String email;
+    private String phone;
+    private LocalDateTime dateRegister;
+
+    //--- Converter UserDTO em User
+    public static UserDTO convertToUser(User user) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setName(user.getName());
+        userDTO.setAddress(user.getAddress());
+        userDTO.setCpf(user.getCpf());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setPhone(user.getPhone());
+        userDTO.setDateRegister(user.getDateRegister());
+        return userDTO;
+    }
+}
